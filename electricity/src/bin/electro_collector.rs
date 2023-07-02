955632bb05cfc848bfd15285a2780be9f5d200a3
David <dtsulik@gmail.com>
2023-07-02 20:11:19 +0400

    refactor - revert main.rs - add separate binary

--- electricity/src/bin/electro_collector.rs
+++ electricity/src/bin/electro_collector.rs
@@ -1,12 +1,34 @@
 use anyhow::{Ok, Result};
 
-use dotenvy::dotenv;
+use lambda_runtime::{service_fn, LambdaEvent};
+
+use serde_json::Value;
+
 use electricity::db::init_client;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    dotenv().expect(".env file not found");
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // this needs to be set to false, otherwise ANSI color codes will
+        // show up in a confusing manner in CloudWatch logs.
+        .with_ansi(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
 
+    let func = service_fn(my_handler);
+    if let Err(e) = lambda_runtime::run(func).await {
+        tracing::error!("Error: {}", e);
+        std::process::exit(1);
+    }
+
+    Ok(())
+}
+
+pub(crate) async fn my_handler(_: LambdaEvent<Value>) -> Result<()> {
     let db_client = init_client().await?;
     let pages = vec![
         String::from("https://elektrodistribucija.rs/planirana-iskljucenja-beograd/Dan_0_Iskljucenja.htm"),
