686943de8a54e41dac84ba1ead3cf770aae517d6
David Tsulaia <dtsulaia@ncl.com>
2023-07-11 16:39:24 +0400

    trace level var

--- electricity/src/bin/electro_collector.rs
+++ electricity/src/bin/electro_collector.rs
@@ -2,6 +2,7 @@ use anyhow::{Ok, Result};
 
 use std::env;
 
+use std::str::FromStr;
 use lambda_runtime::{service_fn, LambdaEvent};
 
 use serde_json::Value;
@@ -10,8 +11,11 @@ use electricity::db::init_client;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+
+    let trace_level = env::var("RAW_DATA_TABLE_NAME").unwrap_or("INFO".to_owned());
+
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
+        .with_max_level(tracing::Level::from_str(&trace_level).unwrap_or(tracing::Level::INFO))
         // disable printing the name of the module in every log line.
         .with_target(false)
         // this needs to be set to false, otherwise ANSI color codes will
